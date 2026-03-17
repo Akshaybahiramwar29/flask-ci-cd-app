910fbe218c96e57910dfd9f109bf1dd31827fb72
Akshaybahiramwar29 <akshay.bahiramwar@gmail.com>
2026-03-17 13:29:46 +0530

    Fix formatting of main check in app.py

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello Akshay! CI/CD is working"
- if __name__=="__main__" :
+if __name__=="__main__":
      app.run(host="0.0.0.0",port=5000)
     
 
